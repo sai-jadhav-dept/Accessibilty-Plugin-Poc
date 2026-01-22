97c9ee55c6da72d77593b530ae09b70c65710266
sai-jadhav-dept <sai.jadhav@deptagency.com>
2026-01-22 19:51:49 +0530

    image getting color converted

--- src/accessibility/index.js
+++ src/accessibility/index.js
@@ -6,6 +6,7 @@ export { default as AccessibilityColorWrapper } from './AccessibilityColorWrappe
 export { default as AccessibleText } from './AccessibleText';
 export { default as AccessibleButton } from './AccessibleButton';
 export { default as AccessibleImage } from './AccessibleImage';
+export { default as FilteredImage } from './FilteredImage';
 
 // Export hooks
 export { useThemeColors } from './useThemeColors';
--- src/screens/Intro.js
+++ src/screens/Intro.js
@@ -1,39 +1,16 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet, SafeAreaView } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import GlobalStyles from '../utils/GlobalStyles';
 import Logo from '../components/Logo';
 import Fonts from '../utils/Fonts';
 import CommonButton from '../components/CommonButton';
 import { useNavigation } from '@react-navigation/native';
 import { heightToDp } from '../utils/Responsive';
-import { useDynamicColors } from '../accessibility';
-import { useAccessibility } from '../accessibility/AccessibilityContext';
+import { useDynamicColors, FilteredImage } from '../accessibility';
 
 export default function Intro() {
     const navigation = useNavigation();
     const colors = useDynamicColors();
-    const { colorInversion, greyscale, lowSaturation, highSaturation, whiteHighContrast, darkHighContrast } = useAccessibility();
-    
-    // Calculate image filter styles
-    const getImageStyle = () => {
-        let filterStyle = {};
-        
-        // Apply greyscale
-        if (greyscale) {
-            filterStyle.tintColor = '#808080'; // Grey tint for greyscale effect
-            filterStyle.opacity = 0.8;
-        }
-        // Apply low saturation (muted colors)
-        else if (lowSaturation) {
-            filterStyle.opacity = 0.6;
-        }
-        // Apply high saturation (more vivid)
-        else if (highSaturation) {
-            filterStyle.opacity = 1;
-        }
-        
-        return filterStyle;
-    };
     
     return (
         <SafeAreaView style={[GlobalStyles.mainContainer, { backgroundColor: colors.defaultBackground }]}>
@@ -41,34 +18,11 @@ export default function Intro() {
                 <Logo />
                 <View style={{ marginTop: heightToDp(4), height: "75%" }}>
                     <View style={styles.imageContainer}>
-                        <Image
-                            resizeMode='contain'
+                        <FilteredImage
                             source={require("../assets/images/medical_care.png")}
-                            style={[styles.mediCare, getImageStyle()]}
+                            style={styles.mediCare}
+                            resizeMode='contain'
                         />
-                        {/* Apply strong color inversion overlay */}
-                        {colorInversion && !greyscale && (
-                            <>
-                                <View style={[styles.imageOverlay, { 
-                                    backgroundColor: 'black',
-                                    opacity: 0.5
-                                }]} />
-                                <View style={[styles.imageOverlay, { 
-                                    backgroundColor: 'white',
-                                    opacity: 0.3
-                                }]} />
-                            </>
-                        )}
-                        {/* Apply greyscale overlay */}
-                        {greyscale && (
-                            <View style={[styles.imageOverlay, { backgroundColor: 'rgba(128, 128, 128, 0.4)' }]} />
-                        )}
-                        {/* Apply high contrast overlay */}
-                        {(whiteHighContrast || darkHighContrast) && !colorInversion && !greyscale && (
-                            <View style={[styles.imageOverlay, { 
-                                backgroundColor: darkHighContrast ? 'rgba(0, 0, 0, 0.3)' : 'rgba(255, 255, 255, 0.2)' 
-                            }]} />
-                        )}
                     </View>
                     <Text style={[GlobalStyles.extralargeText, Fonts.Nunito_700Bold, { marginTop: heightToDp(4), color: colors.primaryTextColor }]}>Manage treatments with ease, on one platform​</Text>
                     <Text style={[GlobalStyles.extralargeText, Fonts.Nunito_700Bold, { marginTop: heightToDp(4), color: colors.primaryTextColor }]}>One dashboard. Complete visibility.​</Text>
@@ -87,7 +41,6 @@ export default function Intro() {
 }
 const styles = StyleSheet.create({
     imageContainer: {
-        position: 'relative',
         alignSelf: 'center',
         height: "55%",
         width: '100%',
@@ -96,12 +49,5 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         height: "100%",
         width: '100%',
-    },
-    imageOverlay: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
     }
 })
